773c15608d64bfd693c79eba31ee394f8f7fc4eb
及川雄太 <oikenfight@gmail.com>
2016-01-05 21:58:19 +0900

    add saveHistory method and create history page

---
+++ Chapter5/HistoryViewController.swift
@@ -0,0 +1,38 @@
+//
+//  HistoryViewController.swift
+//  Calculator
+//
+//  Created by 及川雄太 on 2016/01/05.
+//  Copyright © 2016年 shoeisha. All rights reserved.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func back(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
